5b1049a5debb772ff23e79938e9cafb3448595d7
Denis Morozov <morozovsdenis@gmail.com>
2015-11-24 05:47:36 -0800

    Created grid

--- app/graph.js
+++ app/graph.js
@@ -3,37 +3,29 @@ import cytoscape from 'cytoscape';
 export default class Graph {
     constructor(){
         // Set node and edge data for the graph
+        this.numberOfColumns = 11;
+        this.numberOfRows = 9;
+        this.numberOfNodes = this.numberOfColumns * this.numberOfRows;
         this.data = {
-            nodes: [
-                { data: { id: 'a' } },
-                { data: { id: 'b' } },
-                { data: { id: 'c' } },
-                { data: { id: 'd' } },
-                { data: { id: 'e' } },
-                { data: { id: 'f' } },
-                { data: { id: 'g' } },
-                { data: { id: 'h' } },
-                { data: { id: 'i' } }
-            ],
-
-            edges: [
-                { data: { id: 'ab', weight: 1, source: 'a', target: 'b' } },
-                { data: { id: 'ac', weight: 2, source: 'a', target: 'c' } },
-                { data: { id: 'bd', weight: 3, source: 'b', target: 'd' } },
-                { data: { id: 'be', weight: 4, source: 'b', target: 'e' } },
-                { data: { id: 'cf', weight: 5, source: 'c', target: 'f' } },
-                { data: { id: 'cg', weight: 6, source: 'c', target: 'g' } },
-                { data: { id: 'ah', weight: 7, source: 'a', target: 'h' } },
-                { data: { id: 'hi', weight: 8, source: 'h', target: 'i' } }
-            ]
+            nodes: this.createNodes(),
+            edges: this.createEdges(),
         };
 
         // Draws the Cytoscape graph
         this.cy = cytoscape({
             container: document.getElementById('graph'),
+            // styling for the graph
             style: cytoscape.stylesheet()
                 .selector('node').css({
+                    // nice for debugging the layout
                     'content': 'data(id)'
+                })
+                .selector('.sink').css({
+                    'content': 'Sink',
+                    'background-color': '#000'
+                })
+                .selector('.aggregator').css({
+                    'background-color': '#900'
                 })
                 .selector('edge').css({
                     'width': 4,
@@ -50,16 +42,91 @@ export default class Graph {
             elements: this.data,
             zoomingEnabled: false,
             layout: {
-                name: 'breadthfirst',
-                directed: true,
-                roots: '#a',
-                padding: 15
+                name: 'grid',
+                fit: true,
+                padding: 10,
+                rows: this.numberOfRows,
+                columns: this.numberOfColumns,
             },
             ready: function(){
                 //window.Graph.cy = this;
             }
         });
+
+        // add edge for every nearby node
+
+
     }
+
+    createNodes(){
+        var nodes = [];
+        for(var i = 1; i <= this.numberOfNodes; i++)
+        {
+            nodes.push(
+                {
+                    data:
+                    {
+                        id: i.toString(),
+                        //parent: '',
+                    }
+                }
+            );
+        }
+
+        // set the sink node which would be in the middle of the grid by default
+        var sinkNode = Math.floor(this.numberOfNodes/2);
+        nodes[sinkNode] =
+        {
+            data:
+            {
+                id: (++sinkNode).toString(),
+            },
+            classes: 'sink',
+        };
+        return nodes;
+    }
+
+    /* Connecting */
+    createEdges(){
+        var edges = [];
+        for(var i = 1; i < this.numberOfNodes; i++)
+        {
+            let current = i.toString();
+
+            // if its not the end of the row
+            if( i % this.numberOfColumns != 0 ) {
+                let right = (i + 1).toString();
+                edges.push(
+                    {
+                        data: {
+                            // I guess nodes and edges share the same IDs? Prefixing with 0 is needed for the first 10 IDs
+                            id: (i < 10 ? '0'+current : current) + (i < 10 ? '0'+right : right),
+                            source: current,
+                            target: right,
+                            weight: 5,
+                        }
+                    }
+                );
+            }
+
+            // add the nodes underneath unless index is on the last row
+            if( i <= this.numberOfNodes - this.numberOfColumns) {
+                let bottom = (i+this.numberOfColumns).toString();
+                edges.push(
+                    {
+                        data: {
+                            id: (i < 10 ? '0'+current : current) + bottom,
+                            source: current,
+                            target: bottom,
+                            weight: 5,
+                        }
+                    }
+                );
+            }
+        }
+        return edges;
+    }
+
 
     drawPath(from, to){
         var dijkstra = this.cy.elements().dijkstra(from,function(){
